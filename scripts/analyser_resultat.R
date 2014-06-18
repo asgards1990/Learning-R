8d5dce94d4fb789196cfbe2360b68ae8d3155b00
Su YANG <ys19901010@hotmail.fr>
2014-06-18 17:01:59 +0200

    adding basic structures

---
+++ scripts/analyser_resultat.R
@@ -0,0 +1,11 @@
+# Mise à jour de votre espace de travail comme d'habitude
+setwd("/Users/sya/Desktop/R/")
+
+# On charge la variable que l'on avait précédemment enregistrée
+load("donnees/resultat.RData")
+
+# Et on y applique un nouveau traitement
+nouveau_resultat <- (resultat + 3)^4
+nouveau_resultat <-  sqrt(nouveau_resultat)
+
+print(paste("Le nouveau résultat est: ", nouveau_resultat, sep=""))
